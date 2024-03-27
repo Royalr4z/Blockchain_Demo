c05dcbdde9170db9946f5066c5506e24743dbc63
RoyalR4z <viniciusviana2004@hotmail.com>
2024-03-27 14:43:17 -0300

    :sparkles: feat: Creating Function 'create_block()'

--- Controllers/BlockController.cs
+++ Controllers/BlockController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using BlockchainDemo.Models;
+using System.Collections.Generic;
 using System;
 
 
@@ -9,11 +10,28 @@ namespace BlockchainDemo.Controllers {
     [ApiController]
 
     public class BlockController : ControllerBase {
+    
+        List<Dictionary<string, object>> chain = [];
+
+        private Dictionary<string, object> create_block(int proof, string previous_hash) {
+
+            Dictionary<string, object> block = new Dictionary<string, object>();
+
+            block["index"] = chain.Count;
+            block["timestamp"] = DateTime.Now.ToString();
+            block["proof"] = proof;
+            block["previous_hash"] = previous_hash;
+
+            chain.Add(block);
+
+            return block;
+        }
 
         [HttpGet]
-        public ActionResult Get() {
+        public ActionResult<List<Dictionary<string, object>>> Get() {
+            create_block(1, "0");
 
-            return Ok("teste");
+            return Ok(chain);
         }
 
     }
