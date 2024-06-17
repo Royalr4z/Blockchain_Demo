0acf32f5ed1a0a526f2ee0c375a3d5632b3b1172
RoyalR4z <viniciusviana2004@hotmail.com>
2024-06-17 12:33:59 -0300

    :recycle: refactor: Arranging the Block Structure

--- Models/BlockModel.cs
+++ Models/BlockModel.cs
@@ -5,25 +5,25 @@ namespace BlockchainDemo.Models {
         public int index { get; set; }
         public int uBits { get; set; }
         public int nonce { get; set; }
-        public int confirmations { get; set; }
         public long timestamp { get; set; }
         public string merkleRoot { get; set; }
         public string hash { get; set; }
         public string previous_hash { get; set; }
         public int txnCounter { get; set; }
         public List<TransactionModel> transactions { get; set; }
+        public int confirmations { get; set; }
 
         public BlockModel() {
             index = 0;
             uBits = 0;
             nonce = 0;
-            confirmations = 0;
             timestamp = 0;
             merkleRoot = string.Empty;
             hash = string.Empty;
             previous_hash = string.Empty;
             txnCounter = 0;
             transactions = [];
+            confirmations = 0;
         }
 
     }
